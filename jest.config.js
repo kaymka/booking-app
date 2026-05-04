634d8a549f6ae8ab816b8d648fd6946b9d027bd1
kaymka <158697513+kaymka@users.noreply.github.com>
2026-05-04 15:06:31 +0300

    Add config files

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json']
+};
